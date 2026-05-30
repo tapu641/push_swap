33f9a236e32f44f1b5708aaeb72c081e89dbaf6b
Rin Nagai <rnagai@c5r3s1.42tokyo.jp>
2026-05-30 13:16:29 +0900

    スタック作成追記版

--- push_swap.c
+++ push_swap.c
@@ -6,7 +6,7 @@
 /*   By: rnagai <rnagai@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/25 20:08:05 by rnagai            #+#    #+#             */
-/*   Updated: 2026/05/29 22:25:13 by rnagai           ###   ########.fr       */
+/*   Updated: 2026/05/30 12:51:50 by rnagai           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,7 +29,14 @@ int main(int argc, char **argv)
 	// スタックの作成
 	while (*argv[i] != '\0')
 	{
+		// ソートオプションが設定されていたら戦略を保持するようにする
+		if (*argv[i] == '-' && *argv[i + 1] == '-')
+		{
+			// TODO: オプションが指定されたら、分岐時に条件として使えるように構造体に保持する
+			// handle_option();
+		}
 		// atoiに失敗したらErrorを出力して終了。
+		// TODO: atoiに失敗したらfailure_flagに-1を入れる処理を実装する。
 		tmp = ft_atoi(argv[i], &failure_flag);
 		if (failure_flag == -1)
 		{
